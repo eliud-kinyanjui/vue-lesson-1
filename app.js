95d40b4ed9efb56318d4eacb461455d6c0c3c6df
Eliud Kinyanjui <eliud.kinyanjui@ramani.co.ke>
2022-04-28 10:40:02 +0300

    Vue CDN Lesson 1

---
+++ app.js
@@ -0,0 +1,23 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstname: 'Eliud',
+      lastname: 'Ngugi',
+      email: 'elkingsparxxes@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/women/10.jpg'
+    }
+  },
+  methods: {
+    async getRandomUser() {
+        const res = fetch('https://randomuser.me/api/')
+        const { results } = await (await res).json()
+        // console.log(results);
+        this.firstname = results[0].name.first
+        this.lastname =  results[0].name.last
+        this.email =  results[0].email
+        this.gender =  results[0].gender
+        this.picture =  results[0].picture.large
+    }
+  }
+}).mount('#app')
